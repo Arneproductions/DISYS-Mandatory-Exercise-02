18db7c9f20b68a905d9cb71c52238eb7b1367330
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-10 19:10:58 +0100

    Create a queue

---
+++ internal/collection/queue.go
@@ -0,0 +1,42 @@
+package collection
+
+import (
+	"container/list"
+	"errors"
+)
+
+type Queue struct {
+	list list.List
+}
+
+/*
+* Returns an initialized queue
+ */
+func NewQueue() Queue {
+	return Queue{
+		list: *list.New(),
+	}
+}
+
+func (q *Queue) IsEmpty() bool {
+	return q.list.Len() > 0
+}
+
+func (q *Queue) Push(v interface{}) error {
+	if v == nil {
+		return errors.New("Input is nil!")
+	}
+
+	q.list.PushBack(v)
+	return nil
+}
+
+func (q *Queue) Pop() interface{} {
+	if q.IsEmpty() {
+		return nil
+	}
+
+	// Get the first element in the list
+	ele := q.list.Front()
+	return q.list.Remove(ele) // pop it
+}
