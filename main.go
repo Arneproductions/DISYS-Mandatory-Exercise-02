b01d78cb09771a39a33ed4c249b82af5dfc017a3
albe2669 <albert@risenielsen.dk>
2021-11-11 10:50:41 +0100

    Implemented res handler

--- main.go
+++ main.go
@@ -72,6 +72,7 @@ func (n *Node) StartServer() {
 	}
 }
 
+// Start and join a cluster
 func (n *Node) StartCluster(clusterAddr *string) error {
 	conf := serf.DefaultConfig()
 	conf.Init()
@@ -93,6 +94,14 @@ func (n *Node) StartCluster(clusterAddr *string) error {
 
 // Send Req message
 func (n *Node) GetLock(in *pb.EmptyWithTime) error {
+	// We cannot ask others if we already have the lock / have asked
+	if n.status == Status_WANTED || n.status == Status_HELD {
+		return nil
+	}
+
+	n.status = Status_WANTED
+	n.responses = n.cluster.NumNodes() - 1
+
 	for _, member := range n.cluster.Members() {
 		n.timestamp.Increment()
 
@@ -105,7 +114,6 @@ func (n *Node) GetLock(in *pb.EmptyWithTime) error {
 		defer conn.Close()
 		c := pb.NewDmeApiServiceClient(conn)
 
-		// Contact the server and print out its response.
 		ctx, cancel := context.WithTimeout(context.Background(), goTime.Second)
 		defer cancel()
 
@@ -151,7 +159,16 @@ func (n *Node) Req(ctx context.Context, in *pb.RequestMessage) (*pb.EmptyWithTim
 // Handle incoming Res
 // TODO: Implement handling of release here
 func (n *Node) Res(ctx context.Context, in *pb.EmptyWithTime) (*pb.EmptyWithTime, error) {
+	n.timestamp.Sync(in.GetTime())
 	n.timestamp.Increment()
+
+	// Decrease response count
+	n.responses -= 1
+
+	// If all nodes have responded, we have achieved lock
+	if n.responses == 0 {
+		n.status = Status_HELD
+	}
 
 	return &pb.EmptyWithTime{Time: n.timestamp.GetTime()}, nil
 }
@@ -168,4 +185,6 @@ func (n *Node) Exit() {
 		addr := fmt.Sprintf("%v", n.queue.Dequeue())
 		n.SendRes(addr)
 	}
+
+	n.status = Status_RELEASED
 }
